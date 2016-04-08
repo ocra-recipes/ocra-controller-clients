3da99a7c5106d2063de04b011c70dedc4adf9a72
Ryan Lober <rlober@gmail.com>
2016-04-08 09:53:11 +0200

    More tests

--- test-client/src/TestClient.cpp
+++ test-client/src/TestClient.cpp
@@ -55,11 +55,77 @@ void TestClient::loop()
     std::cout << "task->getTaskErrorNorm()\n" << task->getTaskErrorNorm() << std::endl;
     std::cout << "\n\n";
 
+    // Stiffness
+
+    std::cout << "task->setStiffness(30)\n";
+    task->setStiffness(30);
+    std::cout << "\n\n";
+
     std::cout << "task->getStiffness()\n" << task->getStiffness() << std::endl;
+    std::cout << "\n\n";
+
+    std::cout << "task->setStiffness(Eigen::VectorXd::Ones(3)*20)\n";
+    Eigen::VectorXd kVec = Eigen::VectorXd::Ones(3)*20;
+    task->setStiffness(kVec);
     std::cout << "\n\n";
 
     std::cout << "task->getStiffnessMatrix()\n" << task->getStiffnessMatrix() << std::endl;
     std::cout << "\n\n";
+
+    std::cout << "task->setStiffness(Eigen::MatrixXd::Identity(3,3)*40)\n";
+    Eigen::MatrixXd kMat = Eigen::MatrixXd::Identity(3,3)*40;
+    task->setStiffness(kMat);
+    std::cout << "\n\n";
+
+    std::cout << "task->getStiffnessMatrix()\n" << task->getStiffnessMatrix() << std::endl;
+    std::cout << "\n\n";
+
+
+    // Damping
+
+    std::cout << "task->setDamping(10)\n";
+    task->setDamping(10);
+    std::cout << "\n\n";
+
+    std::cout << "task->getDamping()\n" << task->getDamping() << std::endl;
+    std::cout << "\n\n";
+
+    std::cout << "task->setDamping(Eigen::VectorXd::Ones(3)*5)\n";
+    Eigen::VectorXd bVec = Eigen::VectorXd::Ones(3)*5;
+    task->setDamping(bVec);
+    std::cout << "\n\n";
+
+    std::cout << "task->getDampingMatrix()\n" << task->getDampingMatrix() << std::endl;
+    std::cout << "\n\n";
+
+    std::cout << "task->setDamping(Eigen::MatrixXd::Identity(3,3)*10)\n";
+    Eigen::MatrixXd bMat = Eigen::MatrixXd::Identity(3,3)*10;
+    task->setDamping(bMat);
+    std::cout << "\n\n";
+
+    std::cout << "task->getDampingMatrix()\n" << task->getDampingMatrix() << std::endl;
+    std::cout << "\n\n";
+
+
+
+    // Damping
+
+    std::cout << "task->setWeight(0.5)\n";
+    task->setWeight(0.5);
+    std::cout << "\n\n";
+
+    std::cout << "task->getWeight()\n" << task->getWeight() << std::endl;
+    std::cout << "\n\n";
+
+    std::cout << "task->setWeight(Eigen::VectorXd::Ones(3)*0.0001)\n";
+    Eigen::VectorXd wVec = Eigen::VectorXd::Ones(3)*0.0001;
+    task->setWeight(wVec);
+    std::cout << "\n\n";
+
+    std::cout << "task->getWeight()\n" << task->getWeight() << std::endl;
+    std::cout << "\n\n";
+
+
 
 
 }
